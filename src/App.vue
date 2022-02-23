b650046b426ebcc9d5200991c01acfe9ebfdd601
Arnaud Thomasse <arnaudthomasse@gmail.com>
2022-02-23 10:09:14 +1000

    try

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,7 @@
+<script setup lang="ts">
+import Adjuster from './components/Adjuster.vue';
+</script>
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Adjuster></Adjuster>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
