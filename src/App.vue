e861d2a1bc4d248bc17ad710a1934b7dba4b2ab9
Arnaud Thomasse <arnaudthomasse@gmail.com>
2022-02-23 10:19:37 +1000

    Revert "try"
    This reverts commit b650046b426ebcc9d5200991c01acfe9ebfdd601.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
-<script setup lang="ts">
-import Adjuster from './components/Adjuster.vue';
-</script>
 <template>
-  <Adjuster></Adjuster>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { GesturePlugin } from '@vueuse/gesture'
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
-
-const app = createApp(App);
-app.use(GesturePlugin);
-app.mount('#app')
